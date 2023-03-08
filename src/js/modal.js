d7936682e0eeb243c87312f0897ef3e0c050f66c
GOOHyunSeo <ghs3956@naver.com>
2023-03-08 18:46:46 +0900

    Finished Screens

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,25 @@
 const body = document.querySelector("body");
 const modal = document.querySelector(".modal");
 const btnOpenPopup = document.querySelector(".btn-open-popup");
-const modalClose = document.querySelector(".modal_close");
+const modalCloseBtn = document.querySelector(".modal_close-btn");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onClick() {
   console.log("it is clicked!!!");
 }
 function openModal() {
-  modal.classList.remove("hidden");
+  modal.classList.remove(HIDDEN_CLASSNAME);
+  if (!modal.classList.contains(HIDDEN_CLASSNAME)) {
+    body.style.overflow = "hidden";
+  }
 }
 function closeModal() {
-  modal.classList.add("hidden");
+  modal.classList.add(HIDDEN_CLASSNAME);
+  if (modal.classList.contains(HIDDEN_CLASSNAME)) {
+    body.style.overflow = "auto";
+  }
 }
 
-btnOpenPopup.addEventListener("click", onClick, openModal);
-modalClose.addEventListener("click", onClick, closeModal);
+btnOpenPopup.addEventListener("click", openModal);
+modalCloseBtn.addEventListener("click", closeModal);
