3ad140ad7b4eb5807f6e1e143089ec30f85208b2
GOOHyunSeo <ghs3956@naver.com>
2023-03-09 23:39:40 +0900

    Use <dialog> to make modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,24 +2,8 @@ const body = document.querySelector("body");
 const modal = document.querySelector(".modal");
 const btnOpenPopup = document.querySelector(".btn-open-popup");
 const modalCloseBtn = document.querySelector(".modal_close-btn");
+const dialog = document.querySelector("dialog");
 
-const HIDDEN_CLASSNAME = "hidden";
-
-function onClick() {
-  console.log("it is clicked!!!");
-}
-function openModal() {
-  modal.classList.remove(HIDDEN_CLASSNAME);
-  if (!modal.classList.contains(HIDDEN_CLASSNAME)) {
-    body.style.overflow = "hidden";
-  }
-}
-function closeModal() {
-  modal.classList.add(HIDDEN_CLASSNAME);
-  if (modal.classList.contains(HIDDEN_CLASSNAME)) {
-    body.style.overflow = "auto";
-  }
-}
-
-btnOpenPopup.addEventListener("click", openModal);
-modalCloseBtn.addEventListener("click", closeModal);
+btnOpenPopup.addEventListener("click", () => {
+  dialog.showModal();
+});
