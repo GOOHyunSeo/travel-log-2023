e2e29f56b1165c03cb89372d873bd15fe830210d
GOOHyunSeo <ghs3956@naver.com>
2023-08-25 20:05:44 +0900

    Restart

---
+++ README.md
@@ -0,0 +1,16 @@
+#travel-log
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/place/:id -> See Video
+/place/:id/edit -> Edit Video
+/place/:id/delete -> Delete Video
+/place/upload -> Upload Video
--- src/js/modal.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector("body");
-const modal = document.querySelector(".modal");
-const btnOpenPopup = document.querySelector(".btn-open-popup");
-const modalCloseBtn = document.querySelector(".modal_close-btn");
-const dialog = document.querySelector("dialog");
-
-btnOpenPopup.addEventListener("click", () => {
-  dialog.showModal();
-});
